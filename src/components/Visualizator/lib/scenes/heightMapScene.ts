298d273d3dca970006711cbac1181734b5085d28
Dimitri Teboul <d.teboul@hotmail.fr>
2019-09-25 22:45:40 +0200

    Replace polygon by isobox

--- src/components/Visualizator/lib/scenes/heightMapScene.ts
+++ src/components/Visualizator/lib/scenes/heightMapScene.ts
@@ -55,28 +55,59 @@ export class HeightMapScene extends Phaser.Scene {
     const width = 50;
     const minHeight = 0;
     const maxHeight = 60;
-    const offsetX = 500;
-    const offsetY = 100;
-    for (let y = 0; y < 10; y++) {
-      for (let x = 0; x < 10; x++) {
+    const size = 30;
+    var land = [];
+    const gridWidth = 100;
+    const gridHeight = 100;
+    var centerX = window.innerWidth / 4;
+    var centerY = -100;
+    for (let y = 0; y < gridHeight; y++) {
+      let row = [];
+      for (let x = 0; x < gridWidth; x++) {
         const depth = (width * 2) / 4;
         const halfDepth = depth / 2;
         const halfWidth = width / 2;
         const height = Math.random() * (maxHeight - minHeight) + minHeight;
         const t = (height - minHeight) / (maxHeight - minHeight);
         const color = this.lowColor.lerpTo(this.highColor, t);
-        const cube = new Cube(
-          new Phaser.Geom.Point(
-            (x - y) * halfWidth * 1.02 + offsetX,
-            (x + y) * halfDepth * 1.02 + offsetY
-          ),
+        // const cube = new Cube(
+        //   new Phaser.Geom.Point(
+        //     (x - y) * halfWidth * 1.02 + offsetX,
+        //     (x + y) * halfDepth * 1.02 + offsetY
+        //   ),
+        //   height,
+        //   width,
+        //   color,
+        //   this
+        // );
+        // this.cubeList.push(cube);
+        // this.add.isobox()
+
+        var tx = (x - y) * halfWidth * 0.5;
+        var ty = (x + y) * halfDepth * 0.5;
+
+        var tile = this.add.isobox(
+          centerX + tx,
+          centerY + ty,
+          size,
           height,
-          width,
-          color,
-          this
+          color.toHex(),
+          color.darken(30).toHex(),
+          color.darken(15).toHex()
         );
-        this.cubeList.push(cube);
+
+        tile.setDepth(centerY + ty);
+        tile
+          .setInteractive()
+          .on(
+            'pointerdown',
+            (pointer: any, localX: number, localY: number, event: any) => {
+              console.log('click');
+            }
+          );
+        row.push(tile);
       }
+      land.push(row);
     }
   }
 
