ad8b656419c30656999fdf626ba71e17c00bc652
Philippe Ottenwaelter <pottenwaelter@free.fr>
2019-10-09 19:52:00 +0200

    Add proper map data processing in HeightMapScene

--- src/components/Visualizator/lib/scenes/heightMapScene.ts
+++ src/components/Visualizator/lib/scenes/heightMapScene.ts
@@ -3,6 +3,13 @@ import { Cube } from '../objects/Cube';
 import Color from '../../../../lib/Isometric3DMap/utils/Color';
 import { IIsometric3DGridInputParams } from '../../../../lib/Isometric3DMap/isometric3dgrid';
 import SpacePoint from '../../../../lib/Isometric3DMap/interfaces/spacePoint.interface';
+import MapDataToGrid from '../../../../lib/Isometric3DMap/mapdata/mapdatatogrid';
+import { IMapGridPoint } from '../../../../lib/Isometric3DMap/interfaces/map-grid-point.interface';
+
+interface IMinMaxHeight {
+  minHeight: number;
+  maxHeight: number;
+}
 
 export class HeightMapScene extends Phaser.Scene {
   /**
@@ -11,14 +18,13 @@ export class HeightMapScene extends Phaser.Scene {
   private map: any;
   private params: any;
   private width = 50;
-  private minHeight = 0;
-  private maxHeight = 0;
   private size = 30;
   private land: any[] = [];
   private gridWidth = 100;
   private gridHeight = 100;
   private centerX = window.innerWidth / 4;
-  private centerY = -100;
+  private centerY = 100;
+  private lastPointerCoordinates: { x: number; y: number } = { x: 0, y: 0 };
 
   /**
    * Color Settings
@@ -29,6 +35,7 @@ export class HeightMapScene extends Phaser.Scene {
   private cubeList: any[] = [];
 
   public mapData: SpacePoint[] = [];
+  private mapDataJSON: any = {};
 
   // private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
@@ -37,70 +44,81 @@ export class HeightMapScene extends Phaser.Scene {
       key: 'HeightMapScene'
     });
 
-    this.lowColor = new Color(255, 255, 128);
-    this.highColor = new Color(179, 179, 255);
+    this.highColor = new Color(255, 255, 128);
+    this.lowColor = new Color(179, 179, 255);
   }
 
   init(): void {
-    this.mapData = require('../../data.json').results;
-    const { min, max } = this.getMinMaxHeight(this.mapData);
-
-    this.minHeight = min;
-    this.maxHeight = max;
+    this.mapDataJSON = require('../../data.json');
+    this.mapData = this.mapDataJSON.results;
   }
 
-  private getMinMaxHeight(map: SpacePoint[]) {
+  private getMinMaxHeight(map: IMapGridPoint[]): IMinMaxHeight {
     return map.reduce(
-      (prev, current) => ({
-        min: Math.min(prev.min, current.elevation),
-        max: Math.max(prev.max, current.elevation)
+      (prev: IMinMaxHeight, current: IMapGridPoint) => ({
+        minHeight: Math.min(prev.minHeight, current.height),
+        maxHeight: Math.max(prev.maxHeight, current.height)
       }),
-      { min: 0, max: 0 }
+      { minHeight: 0, maxHeight: 0 }
     );
   }
 
   create(): void {
-    for (let y = 0; y < this.gridHeight; y++) {
-      let row = [];
-      for (let x = 0; x < this.gridWidth; x++) {
-        const depth = (this.width * 2) / 4;
-        const halfDepth = depth / 2;
-        const halfWidth = this.width / 2;
-        let height = 0;
-        if (x + y < this.mapData.length) {
-          height = this.mapData[x + y].elevation;
-        }
-        const t = (height - this.minHeight) / (this.maxHeight - this.minHeight);
-        const color = this.lowColor.lerpTo(this.highColor, t);
+    const gridDataRaw = new MapDataToGrid(this.mapDataJSON.results);
+    const gridData: IMapGridPoint[] = gridDataRaw.getGrid();
 
-        var tx = (x - y) * halfWidth * 0.6;
-        var ty = (x + y) * halfDepth * 0.6;
+    const { minHeight, maxHeight } = this.getMinMaxHeight(gridData);
 
-        let cube = new Cube(
-          new Phaser.Geom.Point(this.centerX + tx, this.centerY + ty),
-          height,
-          this.size,
-          color,
-          this
-        );
+    const n = gridData.length;
+    let row: any = [];
+    for (let i = 0; i < n; i++) {
+      const singleGridData = gridData[i];
+      const { x, y } = singleGridData;
+      const depth = (this.width * 2) / 4;
+      const halfDepth = depth / 2;
+      const halfWidth = this.width / 2;
+      const height = singleGridData.height;
+      const t = (height - minHeight) / (maxHeight - minHeight);
+      const color = this.lowColor.lerpTo(this.highColor, t);
 
-        cube.setDepth(this.centerY + ty);
-        row.push(cube);
-        this.add.existing(cube);
-      }
-      this.land.push(row);
+      var tx = (x - y) * halfWidth * 0.6;
+      var ty = (x + y) * halfDepth * 0.6;
+
+      let cube = new Cube(
+        new Phaser.Geom.Point(this.centerX + tx, this.centerY + ty),
+        0,
+        this.size,
+        color,
+        this
+      );
+
+      cube.setDepth(this.centerY + ty);
+      row.push(cube);
+      this.add.existing(cube);
+
+      const reverseIndex = n - 1 - i;
+      this.tweens.add({
+        targets: cube,
+        height: singleGridData.height,
+        ease: 'Sine.easeOut',
+        duration: 1500 + 10 * reverseIndex,
+        delay: reverseIndex * 2
+      });
     }
+    this.land.push(row);
   }
 
   move_camera_by_pointer(o_pointer: Phaser.Input.Pointer) {
     if (!o_pointer.upTime) {
       return;
     }
-    if (o_pointer.primaryDown) {
-      if (this.cameras.main) {
-        this.cameras.main.x += this.cameras.main.x - o_pointer.position.x;
-        this.cameras.main.y += this.cameras.main.y - o_pointer.position.y;
-      }
+    if (o_pointer.primaryDown && this.cameras.main) {
+      const { x, y } = this.lastPointerCoordinates;
+      this.cameras.main.x += o_pointer.position.x - x;
+      this.cameras.main.y += o_pointer.position.y - y;
+
+      this.lastPointerCoordinates.x = o_pointer.position.x;
+      this.lastPointerCoordinates.y = o_pointer.position.y;
     }
   }
 
