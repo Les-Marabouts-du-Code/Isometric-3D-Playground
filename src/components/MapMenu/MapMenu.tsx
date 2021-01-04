ab26ff0a1a2957b0555f93ea3bd13aaf0b12541f
Philippe Ottenwaelter <philippe.ottenwaelter@elamp.fr>
2021-01-04 15:55:32 +0100

    Add map settings storage

--- src/components/MapMenu/MapMenu.tsx
+++ src/components/MapMenu/MapMenu.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { TileLayer, Rectangle, PathProps } from "react-leaflet";
-import { LatLngLiteral, LatLngBoundsExpression } from "leaflet";
-import CustomMap from "../CustomMap/CustomMap";
+import React from 'react';
+import { TileLayer, Rectangle, PathProps } from 'react-leaflet';
+import {
+  LatLngLiteral,
+  LatLngBoundsExpression,
+  LeafletEvent,
+  DragEndEvent
+} from 'leaflet';
+import CustomMap from '../CustomMap/CustomMap';
 
 interface IMapArea {
   latitude: number;
@@ -12,6 +17,10 @@ interface IMapArea {
 
 interface IMapAreaFile extends IMapArea {
   file: string;
+}
+
+interface IMapInitialSettings extends LatLngLiteral {
+  zoom: number;
 }
 
 type MapMenuProps = {
@@ -25,6 +34,7 @@ const defaultPosition: LatLngLiteral = {
   lat: 51.5,
   lng: -0.09
 };
+const defaultZoom: number = 5
 
 const getBoundsFromMapArea = (area: IMapArea): LatLngBoundsExpression => {
   return [
@@ -33,26 +43,54 @@ const getBoundsFromMapArea = (area: IMapArea): LatLngBoundsExpression => {
   ];
 };
 
+const getMapInitialSettings = (): IMapInitialSettings => {
+  const mapSettingsString: string = localStorage.getItem('mapSettings') || ''
+  let mapSettings: IMapInitialSettings
+  try {
+    mapSettings = JSON.parse(mapSettingsString)
+  } catch(e) {
+    mapSettings = {
+      zoom: defaultZoom, 
+      ...defaultPosition, 
+    }
+  }
+  return mapSettings
+}
+
+const storeMapCurrentSettings = (settings: IMapInitialSettings) => {
+  localStorage.setItem('mapSettings', JSON.stringify(settings))
+}
+
 export default function MapMenu(props: MapMenuProps) {
-  const position: LatLngLiteral = {
-    lat: props.lat ? props.lat : defaultPosition.lat,
-    lng: props.lng ? props.lng : defaultPosition.lng
-  };
+  const { zoom: initialZoom, ...initialPosition } = getMapInitialSettings()
 
   const pathProps: PathProps = {
-    fillColor: "#ffffff",
+    fillColor: '#ffffff',
     stroke: true,
     weight: 4,
-    color: "#d03030"
+    color: '#d03030'
   };
 
   const onClick = (file: string) => () => {
     props.onClickCallback(file);
   };
 
+  const onMapSettingsUpdate = (event: LeafletEvent | DragEndEvent) => {
+    storeMapCurrentSettings({
+      zoom: event.target._zoom, 
+      ...event.target._renderer._center
+    })
+  };
+
   return (
     <>
-      <CustomMap className="map-component" center={position} zoom={5}>
+      <CustomMap
+        className="map-component"
+        center={initialPosition}
+        zoom={initialZoom}
+        onzoomend={onMapSettingsUpdate}
+        ondragend={onMapSettingsUpdate}
+      >
         {props.areas.map((area, index) => (
           <Rectangle
             key={`rectangle_${index}`}
